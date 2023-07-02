c531e91484c93a05b33794f8c3bfefe2fde57bb4
Your Name <you@example.com>
2023-07-02 20:36:45 +0700

    [MOD] set env cert for nginx

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+
 # -------------------------------- Angular ----------------------------------
 IMAGE_NAME="wiimme/service"
 TAG="1.0"
 CONTAINER_NAME="wiimme-service"
 NETWORK="bridge-net"
-PUB_PORT=8080
+PUB_PORT=8443
+DIR_SSL="/server/Ziimme-ui/letsencrypt"
+
 # ---------------------------------------------------------------------------
 sudo docker stop $CONTAINER_NAME
 sudo docker rm   $CONTAINER_NAME
 sudo docker rmi  $IMAGE_NAME:$TAG
+
 # ---------------------------------------------------------------------------
 sudo docker build -t $IMAGE_NAME:$TAG /server/Ziimme-api
 sudo docker run --name $CONTAINER_NAME \
                 --network $NETWORK \
-                -p $PUB_PORT:8080 \
+                -p $PUB_PORT:8443 \
                 --restart=always \
+                -v $DIR_SSL:/letsencrypt \
                 -d $IMAGE_NAME:$TAG
