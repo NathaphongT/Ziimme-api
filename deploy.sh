b427dbe6bc963020deffcf257ea7a7b72a53d92f
Your Name <you@example.com>
2025-03-22 23:02:04 +0700

    fix config and path

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ IMAGE_NAME=ziimme/service
 CONTAINER_NAME=ziimme-service
 PORT=8080
 NETWORK=bridge-net
+PATH_CONFIG="/home/ubuntu/Ziimme-api/src/main/resources/application.properties"
 
 # remove container
 docker rm -f $CONTAINER_NAME
@@ -19,7 +20,7 @@ docker build -t $IMAGE_NAME .
 docker create --name $CONTAINER_NAME -p $PORT:8080 --restart=always --network $NETWORK $IMAGE_NAME
 
 # copy environment
-docker cp ./src/main/resources/application.properties $CONTAINER_NAME:/opt/app
+docker cp $PATH_CONFIG $CONTAINER_NAME:/opt/app
 
 # start container
 docker start $CONTAINER_NAME
