10787f53904ff33b16b7565a378b9bd17881118a
Your Name <you@example.com>
2025-03-22 23:06:10 +0700

    fix image tag

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 # parameter name
 IMAGE_NAME=ziimme/service
+TAG=1.0.0
 CONTAINER_NAME=ziimme-service
 PORT=8080
 NETWORK=bridge-net
@@ -11,13 +12,13 @@ PATH_CONFIG="/home/ubuntu/Ziimme-api/src/main/resources/application.properties"
 docker rm -f $CONTAINER_NAME
 
 # remove images
-docker rmi $IMAGE_NAME
+docker rmi $IMAGE_NAME:$TAG
 
 # build image
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME:$TAG .
 
 # create container
-docker create --name $CONTAINER_NAME -p $PORT:8080 --restart=always --network $NETWORK $IMAGE_NAME
+docker create --name $CONTAINER_NAME -p $PORT:8080 --restart=always --network $NETWORK $IMAGE_NAME:$TAG
 
 # copy environment
 docker cp $PATH_CONFIG $CONTAINER_NAME:/opt/app
